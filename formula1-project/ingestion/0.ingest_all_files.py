4e341447a88e31a1817a912df476cd3bc6a4dba2
ricardo-dantas97 <ricardo-dantas97>
2023-04-23 01:11:57 +0000

    Add notebook to run all ingestion files

---
+++ formula1-project/ingestion/0.ingest_all_files.py
@@ -0,0 +1,42 @@
+# Databricks notebook source
+arguments = {"p_data_source":"Ergast API"}
+
+# COMMAND ----------
+
+result = dbutils.notebook.run("1.ingest_circuits_file", 0, arguments)
+result
+
+# COMMAND ----------
+
+result = dbutils.notebook.run("2.ingest_races_file", 0, arguments)
+result
+
+# COMMAND ----------
+
+result = dbutils.notebook.run("3.ingest_constructors_file", 0, arguments)
+result
+
+# COMMAND ----------
+
+result = dbutils.notebook.run("4.ingest_drivers_file", 0, arguments)
+result
+
+# COMMAND ----------
+
+result = dbutils.notebook.run("5.ingest_results_file", 0, arguments)
+result
+
+# COMMAND ----------
+
+result = dbutils.notebook.run("6.ingest_pitstops_file", 0, arguments)
+result
+
+# COMMAND ----------
+
+result = dbutils.notebook.run("7.ingest_laptimes_file", 0, arguments)
+result
+
+# COMMAND ----------
+
+result = dbutils.notebook.run("8.ingest_qualifying_file", 0, arguments)
+result

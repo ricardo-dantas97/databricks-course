12c7f74f0f406bf020dfd52bf7656b6850f52dd4
ricardo-dantas97 <ricardo-dantas97>
2023-04-23 01:12:58 +0000

    Add files with configs and common functions to modularize project

---
+++ formula1-project/utils/configs.py
@@ -0,0 +1,5 @@
+# Databricks notebook source
+storage_account_name = 'rddatabricks'
+raw_folder_path = f'/mnt/{storage_account_name}/raw'
+processed_folder_path = f'/mnt/{storage_account_name}/processed'
+presentation_folder_path = f'/mnt/{storage_account_name}/presentation'
